34eac0b20e0679b5aaee67b5e051a073ed8d9e50
Sam <CSC308SamB>
2026-02-16 17:41:14 -0500

    Initial Commit

---
+++ SamBostonAssign2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SamBostonAssign2
+//
+//  Created by Boston, Samuel T. on 2/16/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
